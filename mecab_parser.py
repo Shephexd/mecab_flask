eaeb7936e158dc75a2750caced0ff441e0616f0c
Ubuntu <ubuntu@ip-172-31-29-97.ap-northeast-2.compute.internal>
2017-09-22 07:14:52 +0000

    fix bug

--- mecab_parser.py
+++ mecab_parser.py
@@ -1,30 +1,35 @@
-from flask import Flask, session, jsonify
+from flask import Flask, session, request
 from konlpy.tag import Mecab
 from collections import defaultdict
+import json
 
 mecab = Mecab()
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'parser is working'
 
-@app.route('/pos/nouns/<string>')
-def parse_nouns(string):
+@app.route('/pos/nouns/', methods=['POST'])
+def parse_nouns():
     res_dict = dict()
+    string = request.form['text']
     res_dict['res'] = mecab.nouns(string)
-    return jsonify(**res_dict)
+    return json.dumps(res_dict)
 
-@app.route('/pos/morphs/<string>')
-def parse_morphs(string):
+@app.route('/pos/morphs/', methods=['POST'])
+def parse_morphs():
     res_dict = dict()
+    string = request.form['text']
     res_dict['res'] = mecab.morphs(string)
-    return jsonify(**res_dict)
+    return json.dumps(res_dict)
 
-@app.route('/pos/<string>')
-def parse_pos(string):
+@app.route('/pos/', methods=['POST'])
+def parse_pos():
     res_dict = dict()
+    string = request.form['text']
     res_dict['res'] = mecab.pos(string)
-    return jsonify(**res_dict)
+    return json.dumps(res_dict)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
